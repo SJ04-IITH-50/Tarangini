f46710731989089d446eae2832f7c6f52a766280
Suddhu-IITH2004 <suddhu040804@gmail.com>
2024-03-07 00:08:17 +0530

    Login Feature Updated

--- src/Home.js
+++ src/Home.js
@@ -1,23 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
-import { signInWithGooglePopup } from "./utils/firebase.utils";
+import { signInWithGooglePopup, signOutUser } from "./utils/firebase.utils";
 
 function Home() {
+  const [user, setUser] = useState(null);
+  const [showLogout, setShowLogout] = useState(false);
+  const [confirmLogout, setConfirmLogout] = useState(false);
+
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
-    console.log(response);
+    setUser(response.user);
   };
+
+  const handleLogout = () => {
+    if (confirmLogout) {
+      signOutUser();
+      setUser(null);
+      setShowLogout(false);
+      setConfirmLogout(false);
+    } else {
+      setConfirmLogout(true);
+    }
+  };
+
   return (
-    <>
+    <div className="home-container">
+      {user && (
+        <div
+          className="profile-icon top-right"
+          onClick={() => setShowLogout(!showLogout)}
+        >
+          {/* Display profile icon or user's email initial */}
+          {user.photoURL ? (
+            <img
+              src={user.photoURL}
+              alt="Profile"
+              className="profile-image"
+            />
+          ) : (
+            <div className="initials">
+              {user.displayName ? user.displayName[0] : user.email[0]}
+            </div>
+          )}
+        </div>
+      )}
       <div className="logo_tarangini">
-        <img src="tarangini_logo.png" className="logo" alt="Tarangini Logo" />
+        <img
+          src="tarangini_logo.png"
+          className="logo"
+          alt="Tarangini Logo"
+        />
       </div>
-      <button className="login" onClick={logGoogleUser}>Login</button>
+      {user ? (
+        <>
+          <div
+            className="profile-icon"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            {/* Display profile icon or user's email initial */}
+            {user.photoURL ? (
+              <img
+                src={user.photoURL}
+                alt="Profile"
+                className="profile-image"
+              />
+            ) : (
+              <div className="initials">
+                {user.displayName ? user.displayName[0] : user.email[0]}
+              </div>
+            )}
+          </div>
+          {showLogout && (
+            <div className="logout-box">
+              {confirmLogout ? (
+                <div className="confirmation-message">
+                  Are you sure you want to log out?
+                  <button onClick={handleLogout}>Yes</button>
+                  <button onClick={() => setConfirmLogout(false)}>No</button>
+                </div>
+              ) : (
+                <button onClick={handleLogout}>Logout</button>
+              )}
+            </div>
+          )}
+        </>
+      ) : (
+        <button className="login" onClick={logGoogleUser}>
+          Login
+        </button>
+      )}
       <div className="container">
         <div className="centered-text">Welcome to Tarangini</div>
         <div className="centered-text">Development in Progress...</div>
       </div>
-    </>
+    </div>
   );
 }
 
