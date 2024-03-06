5d0d0ff0b1fb0531f9682e05a268b6751849bfa2
aswitha-0112 <aswithavinjam@gmail.com>
2024-03-07 00:48:03 +0530

    Conflicts merged

--- src/Home.js
+++ src/Home.js
@@ -1,36 +1,116 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
-import { signInWithGooglePopup } from "./utils/firebase.utils";
+import { signInWithGooglePopup, signOutUser } from "./utils/firebase.utils";
 
 function Home() {
+  const [user, setUser] = useState(null);
+  const [showLogout, setShowLogout] = useState(false);
+  const [confirmLogout, setConfirmLogout] = useState(false);
+
   const logGoogleUser = async () => {
     const response = await signInWithGooglePopup();
-    console.log(response);
+    setUser(response.user);
   };
+
+  const handleLogout = () => {
+    if (confirmLogout) {
+      signOutUser();
+      setUser(null);
+      setShowLogout(false);
+      setConfirmLogout(false);
+    } else {
+      setConfirmLogout(true);
+    }
+  };
+
   return (
-    <>
-      <div className="logo_tarangini">
-        <img src="tarangini_logo.png" className="logo" alt="Tarangini Logo" />
-      </div>
-      <button className="login" onClick={logGoogleUser}>Login</button>
-        <div className="centered-text">Welcome to Tarangini</div>
-        <div style={{display:"flex",justifyContent:"center"}}>
-        <img src="Solar_panels_image.png" className="solar_panels" alt="Solar_panels Image"/>
+    <div className="home-container">
+      {user && (
+        <div
+          className="profile-icon top-right"
+          onClick={() => setShowLogout(!showLogout)}
+        >
+          {/* Display profile icon or user's email initial */}
+          {user.photoURL ? (
+            <img
+              src={user.photoURL}
+              alt="Profile"
+              className="profile-image"
+            />
+          ) : (
+            <div className="initials">
+              {user.displayName ? user.displayName[0] : user.email[0]}
+            </div>
+          )}
         </div>
-        <div style={{display:"flex",justifyContent:"center"}}>
+      )}
+      <div className="logo_tarangini">
+        <img
+          src="tarangini_logo.png"
+          className="logo"
+          alt="Tarangini Logo"
+        />
+      </div>
+      {user ? (
+        <>
+          <div
+            className="profile-icon"
+            onClick={() => setShowLogout(!showLogout)}
+          >
+            {/* Display profile icon or user's email initial */}
+            {user.photoURL ? (
+              <img
+                src={user.photoURL}
+                alt="Profile"
+                className="profile-image"
+              />
+            ) : (
+              <div className="initials">
+                {user.displayName ? user.displayName[0] : user.email[0]}
+              </div>
+            )}
+          </div>
+          {showLogout && (
+            <div className="logout-box">
+              {confirmLogout ? (
+                <div className="confirmation-message">
+                  Are you sure you want to log out?
+                  <button onClick={handleLogout}>Yes</button>
+                  <button onClick={() => setConfirmLogout(false)}>No</button>
+                </div>
+              ) : (
+                <button onClick={handleLogout}>Logout</button>
+              )}
+            </div>
+          )}
+        </>
+      ) : (
+        <button className="login" onClick={logGoogleUser}>
+          Login
+        </button>
+      )}
+      <div className="centered-text">Welcome to Tarangini</div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img src="Solar_panels_image.png" className="solar_panels" alt="Solar_panels Image" />
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         {/* <img src="Battery_image.png" className="Battery" alt="Battery Image"/> */}
         <img src="home_image.png" className="home" alt="Home Image" />
         {/* <img src="grid-removebg-preview.png" className="grid" alt="Grid Image" /> */}
-        </div>
-        <div style={{display:"flex",justifyContent:"center"}}>
-        <img src="Battery_image.png" className="Battery" alt="Battery Image"/>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <img src="Battery_image.png" className="Battery" alt="Battery Image" />
         {/* <img src="home_image.png" className="home" alt="Home Image" /> */}
         <img src="grid-removebg-preview.png" className="grid" alt="Grid Image" />
-        </div>
-        <div className="centered-text">Development in Progress...</div>
-
-    </>
+      </div>
+      <div className="centered-text">Development in Progress...</div>
+    </div>
   );
 }
 
 export default Home;
+
+
+
+
+
--- src/utils/firebase.utils.js
+++ src/utils/firebase.utils.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut, GoogleAuthProvider } from "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCyBYKFy7yTp0BM-D2X3dizMfLT4nAvRJA",
   authDomain: "tarangini-da482.firebaseapp.com",
@@ -14,12 +15,15 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
 // Initialize Firebase Auth provider
 const provider = new GoogleAuthProvider();
   
 // whenever a user interacts with the provider, we force them to select an account
 provider.setCustomParameters({   
-    prompt : "select_account "
+    prompt: "select_account"
 });
+
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+export const signOutUser = () => signOut(auth);
