5cf6c45da0fbd6f9541d00d9042c16264fd06aa6
aswitha-0112 <aswithavinjam@gmail.com>
2024-03-07 00:29:45 +0530

    Added_images

--- src/Home.js
+++ src/Home.js
@@ -14,8 +14,21 @@ function Home() {
       </div>
       <button className="login" onClick={logGoogleUser}>Login</button>
         <div className="centered-text">Welcome to Tarangini</div>
+        <div style={{display:"flex",justifyContent:"center"}}>
+        <img src="Solar_panels_image.png" className="solar_panels" alt="Solar_panels Image"/>
+        </div>
+        <div style={{display:"flex",justifyContent:"center"}}>
+        {/* <img src="Battery_image.png" className="Battery" alt="Battery Image"/> */}
+        <img src="home_image.png" className="home" alt="Home Image" />
+        {/* <img src="grid-removebg-preview.png" className="grid" alt="Grid Image" /> */}
+        </div>
+        <div style={{display:"flex",justifyContent:"center"}}>
+        <img src="Battery_image.png" className="Battery" alt="Battery Image"/>
+        {/* <img src="home_image.png" className="home" alt="Home Image" /> */}
+        <img src="grid-removebg-preview.png" className="grid" alt="Grid Image" />
+        </div>
         <div className="centered-text">Development in Progress...</div>
-        <img src="grid-removebg-preview.png" className="gris" alt="Grid Image" />
+
     </>
   );
 }
