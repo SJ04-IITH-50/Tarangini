50874f1c17b788470f99916a20c862b53d48aca4
aswitha-0112 <aswithavinjam@gmail.com>
2024-03-08 19:13:25 +0530

    Just Pushing to pull

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes,useLocation} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import Home from './Home';
@@ -8,14 +8,21 @@ import HomeBeforeLogin from './HomeBeforeLogin';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom';
+import Header from './components/Header';
 
 
+const UserDataExtract=()=>{
+  const location = useLocation();
+  const userData = location.state && location.state.user ? JSON.parse(location.state.user) : null;
+  console.log(userData)
+};
 const RootComponent = () => {
-
+  
   return (
     <>
+    <Header/>
       <Router>
-        
+        <UserDataExtract/>
         <Routes>
           <Route path="/" element={<HomeBeforeLogin/>} />
           <Route path="/home" element={<Home/>} />
