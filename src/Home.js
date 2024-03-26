48d7630e77049e25b5b59d9eb3bbe57200b01b0f
aswitha-0112 <aswithavinjam@gmail.com>
2024-03-26 05:44:29 +0530

    Home_page

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,10 @@ import "./Home.css";
 import { signInWithGooglePopup, signOutUser } from "./utils/firebase.utils";
 import { useLocation, useNavigate } from "react-router-dom";
 import DownwardArrow from "./components/Arrows/DownwardArrow";
+// import DownwardLeft from "./components/Arrows/DownwardLeft";
 import UpwardArrow from "./components/Arrows/UpwardArrow";
+import DownwardArrowLeft from "./components/Arrows/DownwardArrowLeft";
+import DownwardArrowRight from "./components/Arrows/DownwardArrowRight";
 import {
   getFirestore,
   getDoc,
@@ -13,6 +16,7 @@ import {
 } from "firebase/firestore";
 import { db } from "./utils/firebase.utils";
 import Button from "@mui/material/Button";
+import DownwardLeft from "./components/Arrows/DownwardArrowLeft";
 
 //To update the data of the user by Email
 async function updateUserByEmail(email, newData) {
@@ -75,15 +79,7 @@ function Home() {
     location.state && location.state.user
       ? JSON.parse(location.state.user)
       : null;
-  // console.log(userData);
-  // useEffect(() => {
-  //   if (!userData) {
-  //     // console.log(userData)
-  //     navigate("/"); // Navigate to the home page if user data is null
-  //   }
-  // }, [userData, navigate]);
 
-  // const user_data=getUserByEmail(userData.email)
 
   useEffect(() => {
     if (userData) {
@@ -112,55 +108,10 @@ function Home() {
     }
   }, [userData, navigate]);
 
-  // const [user, setUser] = useState(userData);
-  // const [showLogout, setShowLogout] = useState(false);
-  // const [confirmLogout, setConfirmLogout] = useState(false);
-
-  // const handleLogout  = () => {
-  //   if (confirmLogout) {
-  //     signOutUser();
-  //     setUser(null);
-  //     setShowLogout(false);
-  //     setConfirmLogout(false);
-  //     navigate("/")
-  //   } else {
-  //     setConfirmLogout(true);
-  //   }
-  // };
   console.log(grid);
 
   return (
     <div className="home-container">
-      {/* <div className="logo_tarangini">
-        <img src="tarangini_logo.png" className="logo" alt="Tarangini Logo" />
-      </div>
-      {user ? (
-        <>
-          <div className="profile-icon" onClick={() => setShowLogout(!showLogout)}>
-            {user.photoURL ? (
-              <img src={user.photoURL} alt="Profile" className="profile-image" />
-            ) : (
-              <div className="initials">{user.displayName ? user.displayName[0] : user.email[0]}</div>
-            )}
-          </div>
-          {showLogout && (
-            <div className="logout-box">
-              {confirmLogout ? (
-                <div className="confirmation-message">
-                  Are you sure you want to log out?
-                  <button onClick={handleLogout}>Yes</button>
-                  <button onClick={() => setConfirmLogout(false)}>No</button>
-                </div>
-              ) : (
-                <button onClick={handleLogout}>Logout</button>
-              )}
-            </div>
-          )}
-        </>
-      ) : (
-        <button className="login" onClick={() => navigate("/login")}>Login</button>
-      )} */}
-      {/* <div className="centered-text">Welcome to Tarangini</div> */}
 
       <div style={{ display: "flex", justifyContent: "center" }}>
         <img
@@ -169,62 +120,45 @@ function Home() {
           alt="Solar_panels Image"
         />
       </div>
-      <div style={{ display: "flex", justifyContent: "center",color:"white",fontSize:"10px" }}>
-      <div>Energy Produced:{Isp}kW</div>
+      <div style={{ display: "flex", justifyContent: "center", color: "white", fontSize: "12px", paddingTop:"2%" }}>
+        <div>Energy Produced:{Isp}kW</div>
       </div>
-      {/* {userData && (
-          <div className="centered-text">
-            Current Produced: {Isp} units
-          </div>
-        )} */}
 
-      {/* <div className="scroll-prompt">
-        <div className="scroll-prompt-arrow-container">
-          <div className="scroll-prompt-arrow"><div></div></div>
-          <div className="scroll-prompt-arrow"><div></div></div>
-        </div>
-      </div> */}
-
-      {/* <DownwardArrow /> */}
-      <div className="vertical-lin"></div>
-
-      <div className="vertical-line"></div>
+      <DownwardArrow />
+      {/* <div className="vertical-lin"></div> */}
+      {/* <div className="vertical-line"></div> */}
       <div
-        style={{ display: "flex", justifyContent: "center", paddingTop: "33%", paddingBottom:"10%" }}
+        style={{ display: "flex", justifyContent: "center", paddingTop: "15%" }}
       >
         <img src="logo_white.png" className="logo" alt="Tarangini" />
       </div>
-      <div className="vertical-lin-left"></div>
-      <div className="vertical-line-left"></div>
+      <DownwardArrowLeft/>
       {grid >= 0 ? (
-        <div>
-        <div className="vertical-lin-right-down"></div>
-        <div className="vertical-line-right-down"></div>
-        </div>
+        <DownwardArrowRight />
       ) : (
-        <div className="vertical-line-right-up"></div>
+        <UpwardArrow/>
       )}
-      <div style={{ display: "flex", justifyContent: "center",}}>
+      <div style={{ display: "flex", justifyContent: "center", }}>
         <div>
-        <img src="home_image.png" className="home" alt="Home Image" style={{width:"50%",height:"50%",marginTop:"10%"}}/>
-      <div  style={{width:"30vw",color:"white",fontSize:"10px",paddingLeft:"10%"}}>Energy Consumed: {IH} kW</div>
-      </div>
-      <div>
+          <img src="home_image.png" className="home" alt="Home Image" style={{ width: "50%", height: "50%", marginTop: "5%" }} />
+          <div style={{ width: "30vw", color: "white", fontSize: "10px", paddingLeft: "10%", paddingTop:"2%" }}>Energy Consumed: {IH} kW</div>
+        </div>
+        <div>
           <img
             src="grid-removebg-preview.png"
             className="grid"
             alt="Grid Image"
-            style={{width:"65%",height:"65%",marginTop:"10%"}}
+            style={{ width: "65%", height: "65%",  }}
           />
           {grid >= 0 ? (
-            <div style={{width:"30vw",color:"white",fontSize:"10px",paddingLeft:"25%"}}>Energy to Grid: {IG }kW</div>
+            <div style={{ width: "30vw", color: "white", fontSize: "10px", paddingLeft: "25%" }}>Energy to Grid: {IG}kW</div>
           ) : (
-            <div style={{width:"30vw",color:"white",fontSize:"10px",paddingLeft:"25%"}}>Energy from Grid: {IG} kW</div>
+            <div style={{ width: "30vw", color: "white", fontSize: "10px", paddingLeft: "25%" }}>Energy from Grid: {IG} kW</div>
           )}
-         </div>
+        </div>
       </div>
       <div style={{ display: "flex", justifyContent: "center" }}>
-      
+
 
       </div>
 
